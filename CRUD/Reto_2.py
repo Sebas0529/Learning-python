6f804172a5bbae617162bf943df00ef28fa858d6
sebas <sebashenao0529@gmail.com>
2023-04-05 16:43:53 -0500

    modificacion al reto 2

--- CRUD/Reto_2.py
+++ CRUD/Reto_2.py
@@ -1,17 +1,28 @@
 import requests
 import numpy as np
-import pandas as pd
 import json 
 
 r = requests.get('https://coderbyte.com/api/challenges/json/age-counting')
 r = json.loads(r.text)['data']
 r = [l for l in r.split(',')]
+
+
 total = 0
 
 for n in r:
     if n.startswith(" age="):
         if int(n[5::]) >= 50:
             total += 1
-
+            
+print('*'*100)
 print(f'el total de {total} usuarios son mayores o iguales a 50 años')
+print('*'*100)
+
+
+group =[]
+for i in range(0,len(r),2):
+    n = ((r[i]+','+r[i+1]))
+    list=dict((l.split('=') for l in n.split(',')))
+    group.append(list)   
+print(group)
 #print(len(r.json()['data']))
